d9096240e1f4c4b9be82e5916de20610d7ee8964
faizaqila <m.faiz.a2006@gmail.com>
2024-12-12 08:32:02 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -6,10 +6,28 @@ public class Nomor1 {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
+        System.out.println("Angka ganjil dari 0 hingga " + n + " adalah:");
+        for (int i = 1; i <= n; i += 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println(); 
+    }
 
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Masukkan bilangan bulat n: ");
+        int n = scanner.nextInt();
+
+        
+        tampilkanAngkaGanjil(n);
+
+        
+        scanner.close();
+    }
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
-    }
     
 }
